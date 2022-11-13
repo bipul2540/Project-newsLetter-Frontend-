4d3923abe18b6cc8921ace8ff6b2b8a4dc3a4172
bipulkumar254 <bipulkumar2540@yahoo.com>
2022-11-14 01:11:59 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload = function digital_fn() {
+  // toggle button
+  let toggle = document.querySelector(".btn-toggle");
+  let body = document.querySelector(".body");
+  let nav = document.querySelector(".nav-area");
+  toggle.addEventListener("click", () => {
+    console.log("clicked");
+    if (toggle.classList.contains("fa-bars")) {
+      toggle.classList.replace("fa-bars", "fa-times");
+      nav.classList.add("active");
+      body.classList.add("disable-scroll");
+    } else {
+      toggle.classList.replace("fa-times", "fa-bars");
+      nav.classList.remove("active");
+      body.classList.remove("disable-scroll");
+    }
+  });
+};
