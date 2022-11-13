f9b855d9bef961dd3e3a9746c95040c2cf25b618
bipulkumar254 <bipulkumar2540@yahoo.com>
2022-11-14 01:41:53 +0530

    first commit2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.onload = function digital_fn() {
   // toggle button
   let toggle = document.querySelector(".btn-toggle");
   let body = document.querySelector(".body");
+  let html = document.querySelector(".html")
   let nav = document.querySelector(".nav-area");
   toggle.addEventListener("click", () => {
     console.log("clicked");
@@ -9,10 +10,12 @@ window.onload = function digital_fn() {
       toggle.classList.replace("fa-bars", "fa-times");
       nav.classList.add("active");
       body.classList.add("disable-scroll");
+      html.classList.add("disable-scroll");
     } else {
       toggle.classList.replace("fa-times", "fa-bars");
       nav.classList.remove("active");
       body.classList.remove("disable-scroll");
+      html.classList.remove("disable-scroll");
     }
   });
 };
